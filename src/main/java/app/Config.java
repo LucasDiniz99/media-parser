5281d04e36dde17ee966b71cb9c57a430af9030f
Lucas Rodrigues Diniz <lucas.rodrigues.rdiniz@gmail.com>
2021-11-09 21:43:51 -0300

    adicao das documentacoes e de uma maneira de medir o progresso da leitura do arquivo

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -9,11 +9,15 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 /**
- *
+ * Class with configurations of the program
  * @author lucas
  */
 public class Config {
-    
+    /**
+     * Default float number conversion for this program
+     * @param number Float the number to be converted to string
+     * @return String the resulting converted number
+     */
     public static String numberFormat(float number) {
         NumberFormat format = NumberFormat.getNumberInstance(Locale.UK);
         format.setMaximumFractionDigits(3);
--- src/main/java/app/Media.java
+++ src/main/java/app/Media.java
@@ -8,9 +8,11 @@ package app;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang.StringEscapeUtils;
 
 /**
- *
+ * Class that encapsulate the media data and methods, can be used either to
+ * store the media, parse a media from string and also to print it's xml format
  * @author lucas
  */
 public class Media {
@@ -34,24 +36,23 @@ public class Media {
     }
 
     /**
-     * Parses a line and adds as a new alt title for this media (a valid alt title
- is required to work as intended)
-     *
-     * @param line
+     * Parses a line and adds as a new alt title for this media (a valid alt 
+     * title is required to work as intended)
+     * @param line String the line that will be parsed as an alternative name
+     * for this media
      */
     public void addAltName(String line) {
         this.altNames.add(line.substring(1, line.length() - 1));
     }
 
     /**
-     * Method that checks if a line is a media, media alt title or invalid
-     *
+     * Method that checks if a line is a media, media alt title or invalid data
      * @param line
-     * @return 1 for valid media, 2 for valid alt title, 0 for invalid
+     * @return 1 for valid media, 2 for valid alt title, 0 for invalid  media
      */
     public static int checkLine(String line) {
         // Checks for a valid media
-        Pattern pattern = Pattern.compile(".+ -");
+        Pattern pattern = Pattern.compile("[|]*(.+[ ]*) -([0-9]+)-");
         Matcher matcher = pattern.matcher(line);
         if (matcher.find()) {
             return 1;
@@ -66,6 +67,11 @@ public class Media {
         return 0;
     }
 
+    /**
+     * Method that parses a string and converts it into a media
+     * @param data String the string with the media to be parsed
+     * @return Media a instance of the Media class
+     */
     public static Media parse(String data) {
         // Generate the pattern
         Pattern pattern = Pattern.compile("[|]*(.+[ ]*) -([0-9]+)-[ ]*([ªº*]*)");
@@ -95,15 +101,20 @@ public class Media {
         return null;
     }
 
+    /**
+     * Override of the default toString method, used for converting this
+     * object into a xml equivalent
+     * @return String xml formatted object representing this media
+     */
     @Override
     public String toString() {
         String names = "";
         for(String name : this.altNames) {
-            names += String.format("<name>%s</name>", name);
+            names += String.format("<name>%s</name>", StringEscapeUtils.escapeHtml(name));
         }
         
         return String.format("<media><title>%s</title><altnames>%s</altnames><chapters>%d</chapters><score>%.2f</score></media>",
-                this.title,
+                StringEscapeUtils.escapeHtml(this.title),
                 names,
                 this.chapters,
                 this.score
